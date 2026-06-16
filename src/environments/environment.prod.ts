03c8441fc5aa7b8969027009c0e943be1e33b416
Sandesh <sandeshgharge@gmail.com>
2026-06-16 11:24:56 +0200

    url error fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  backendAiApiURL: 'https://job-apply-backend-ai.onrender.com',
+  backendAiApiURL: 'https://job-apply-backend-ai.onrender.com/',
   PROFILE_ASSETS_BUCKET: 'profile-assets',
   JOB_APPLICATION_BUCKET: 'apply-docs'
 };
